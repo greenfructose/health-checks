5f51eb48de1627b9c069a415d8d788e57c684fef
greenfructose <justinclarkturney@gmail.com>
2020-09-22 14:53:11 -0500

    Added network check to resolve google.com

--- all_checks.py
+++ all_checks.py
@@ -2,6 +2,7 @@
 import os
 import shutil
 import sys
+import socket
 
 def check_reboot():
     """Returns True if the computer has a pending reboot."""
@@ -17,14 +18,24 @@ def check_disk_full(disk, min_gb, min_percent):
     if percent_free < min_percent or gigabytes_free < min_gb:
         return True
     return False
+
 def check_root_full():
     """Returns True if the root partition is full, False otherwise"""
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
+def check_no_network():
+    """Returns True if it fails to resolve Google's URL, False otherwise"""
+    try:
+        socket.gethostbyname("www.google.com")
+        return False
+    except:
+        return True
+
 def main():
     checks=[
         (check_reboot, "Pending Reboot"),
-        (check_root_full, "Root partition full")
+        (check_root_full, "Root partition full"),
+        (check_no_network, "No working network")
     ]
     everything_ok = True
     for check, msg in checks:
